e3047ad13701fa4f8107ae0eef30e2eae2fe799c
Leonardo Campos <Leonardocamposdev777@gmail.com>
2025-02-03 21:42:27 -0300

    update

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,3 +1,13 @@
 export default function initMenuMobile(){
+    const btnMobile = document.querySelector('[data-menu="button"]');
+    const menuList = document.querySelector('[data-menu="list"]');
+    
+    function handleClick(){
+      menuList.classList.add('active');
+      btnMobile.classList.add('active');
+    }
+    
+    btnMobile.addEventListener('click', handleClick);
+  }
 
-}
+
