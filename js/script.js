456b4b3e3f68083610286729a61546f02ffc7924
Leonardo Campos <Leonardocamposdev777@gmail.com>
2025-02-03 22:03:37 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 import initMenuMobile  from "./modules/menu-mobile.js";
 import initIntroducaoCarrosel from "./modules/introducao-carrosel.js";
+import initOutsideClick from "./modules/outsideClick.js";
 
 
 initMenuMobile();
+initOutsideClick()
