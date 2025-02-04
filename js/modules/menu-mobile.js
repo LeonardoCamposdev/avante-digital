304c152fee6544dcefd5039f3de5f15e67afa137
Leonardo Campos <Leonardocamposdev777@gmail.com>
2025-02-03 22:48:50 -0300

    update

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,13 +1,21 @@
+import outsideClick from "./outsideClick.js";
 export default function initMenuMobile(){
     const btnMobile = document.querySelector('[data-menu="button"]');
     const menuList = document.querySelector('[data-menu="list"]');
+    const events = ["click"];
     
     function handleClick(){
       menuList.classList.add('active');
       btnMobile.classList.add('active');
+
+      outsideClick(this,events,()=>{
+        menuList.classList.remove('active');
+        btnMobile.classList.remove('active');
+      })
     }
     
     btnMobile.addEventListener('click', handleClick);
+    menuList.addEventListener('click',handleClick);
   }
 
 
--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 import initMenuMobile  from "./modules/menu-mobile.js";
 import initIntroducaoCarrosel from "./modules/introducao-carrosel.js";
-import initOutsideClick from "./modules/outsideClick.js";
 
 
 initMenuMobile();
-initOutsideClick()
