c6349318ed529072f982e58c9419a3cd797f7f7a
Leonardo Campos <Leonardocamposdev777@gmail.com>
2025-03-24 19:53:35 -0300

    up

--- js/modules/animacao-numeros.js
+++ js/modules/animacao-numeros.js
@@ -1,43 +1,33 @@
-export default function initAnimacaoNumeros(){
-
-}
-
-function animaNumeros(){
-    const numeros = document.querySelectorAll('[data-numero]');
-    
-    numeros.forEach((numero)=>{
-        const total = +numero.innerText;
-        const incremento = Math.floor(total / 100);
-
-        let start = 0;
-        const timer = setInterval(()=>{
-            start = start + incremento;
-            numero.innnerText = start;
-            if(start > total){
-                console.log(start)
-                numero.innerText = total;
-                clearInterval(timer);
-            }
-        },25 * Math.random());
-    })
-}
-
-animaNumeros()
-
-function animaNumeros() {
+export default function initAnimacaoNumeros() {
+  function animaNumeros() {
     const numeros = document.querySelectorAll("[data-numero]");
-    numeros.forEach((numero) => {
-        const total = +numero.innerText;
-        const incremento = Math.floor(total / 100)
 
-        let start = 0
-        const timer = setInterval(() => {
-            start = start + incremento;
-            numero.innerText = start;
-            if (start > total) {
-                numero.innerText = total;
-                clearInterval(timer);
-            }
-        }, 25 * Math.random());
-    })
+    numeros.forEach((numero) => {
+      const total = +numero.innerText;
+      const incremento = Math.floor(total / 100);
+
+      let start = 0;
+      const timer = setInterval(() => {
+        start = start + incremento;
+        numero.innerText = start;
+        if (start > total) {
+          console.log(start);
+          numero.innerText = total;
+          clearInterval(timer);
+        }
+      }, 28 * Math.random());
+    });
+  }
+
+  function handleMutatiton(mutation) {
+    if (mutation[0].target.classList.contains("ativo")) {
+      observer.disconnect();
+      animaNumeros();
+    }
+  }
+
+  const observerTarget = document.querySelector(".gestao-numeros"); // ESTAMOS MARCANDO QUEM VAMOS OBSERVAR
+  const observer = new MutationObserver(handleMutatiton);
+
+  observer.observe(observerTarget, { attributes: true });
 }
