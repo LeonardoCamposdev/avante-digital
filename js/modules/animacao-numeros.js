7ee7f0b61e1189aa3a5d94dd7bd01e999ad4a753
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-03-26 11:37:46 -0300

    up

--- js/modules/animacao-numeros.js
+++ js/modules/animacao-numeros.js
@@ -1,32 +1,11 @@
 export default function initAnimacaoNumeros() {
-  function animaNumeros() {
-    const numeros = document.querySelectorAll("[data-numero]");
-
-    numeros.forEach((numero) => {
+  function animaNumeros(){
+    const numeros = document.querySelectorAll('[data-numero]');
+    numeros.forEach((numero)=>{
       const total = +numero.innerText;
-      const incremento = Math.floor(total / 100);
-
-      let start = 0;
-      const timer = setInterval(() => {
-        start = start + incremento;
-        numero.innerText = start;
-        if (start > total) {
-          numero.innerText = total;
-          clearInterval(timer);
-        }
-      }, 27 * Math.random());
-    });
+      console.log(total);
+    })
   }
 
-  function handleMutatiton(mutation) {
-    if (mutation[0].target.classList.contains("ativo")) {
-      observer.disconnect();
-      animaNumeros();
-    }
-  }
-
-  const observerTarget = document.querySelector(".gestao-numeros"); // ESTAMOS MARCANDO QUEM VAMOS OBSERVAR
-  const observer = new MutationObserver(handleMutatiton);
-
-  observer.observe(observerTarget, { attributes: true });
+  animaNumeros();
 }
