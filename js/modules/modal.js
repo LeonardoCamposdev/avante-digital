439c23b7535a9ffc7544e093f52c6539fed67a17
Leonardo Campos <Leonardocamposdev777@gmail.com>
2025-04-03 20:55:54 -0300

    update

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,13 +1,14 @@
-export default function initModal(){
-    const btnEnviar = document.querySelector('[data-modal="enviar"]');
-    const modalContainer = document.querySelector('.modal-container')
-    
-    function openModal(event){
-        event.preventDefault();
-    
-        modalContainer.classList.add('ativo');
-    }
-    
-    btnEnviar.addEventListener('click',openModal);
-}
+export default function initModal() {
+  const btnEnviar = document.querySelector('[data-modal="enviar"]');
+  const modalContainer = document.querySelector(".modal-container");
 
+  if (btnEnviar && modalContainer) {
+    function openModal(event) {
+      event.preventDefault();
+
+      modalContainer.classList.add("ativo");
+    }
+
+    btnEnviar.addEventListener("click", openModal);
+  }
+}
