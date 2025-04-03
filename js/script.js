0503d1b1a83a3527787ed14d3079ffd1642de240
Leonardo Souza Campos <leonardokiss845@gmail.com>
2025-04-03 17:00:14 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 import initMenuMobile  from "./modules/menu-mobile.js";
 import initPluginScroll from "./modules/plugin-scroll.js";
 import initAnimacaoNumeros from "./modules/animacao-numeros.js";
+import initModal from "./modules/modal.js";
 import { initAnimacaoScrollLeft, initAnimacaoScrollRight, initAnimacaoScrollUp, initAnimacaoScrollDown } from "./modules/animacao-scroll.js";
 
 
 initMenuMobile(); 
 initPluginScroll();
 initAnimacaoNumeros();
+initModal();
 initAnimacaoScrollLeft();
 initAnimacaoScrollRight();
 initAnimacaoScrollUp();
